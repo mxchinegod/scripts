da6b26faca32fda67493ab7ccedbe88f8861dcb8
Dylan <dylan.moore@charter.com>
2022-12-02 15:14:16 -0500

    Added the first baby

---
+++ kubectl/kubeNuke.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+NAMESPACE=$1
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+printf "${RED}Clever delete of "$1" commenced \n${NC}"
+kubectl get namespace $NAMESPACE -o json > $NAMESPACE.json
+sed -i -e 's/"kubernetes"//' $NAMESPACE.json
+kubectl replace --raw "/api/v1/namespaces/$NAMESPACE/finalize" -f ./$NAMESPACE.json
+printf "\n ${RED}Got that disgusting shit out of here... you may continue." 
