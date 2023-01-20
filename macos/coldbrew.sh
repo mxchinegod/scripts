10fe53dcf2e20759e8062bdc34f59a8e886ff586
Dylan <dylan.moore@charter.com>
2023-01-20 11:45:26 -0500

    Colorized

--- macos/coldbrew.sh
+++ macos/coldbrew.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
-GREEN="\e[32m"
+PURPLE="\e[35m"
 ENDCOLOR="\e[0m"
 
-green () {
-    echo -e $(printf "${GREEN}$1${ENDCOLOR}")
+purple () {
+    echo -e $(printf "${PURPLE}$1${ENDCOLOR}")
+}
+
+__dock_item() {
+    printf '%s%s%s%s%s' \
+           '<dict><key>tile-data</key><dict><key>file-data</key><dict>' \
+           '<key>_CFURLString</key><string>' \
+           "$1" \
+           '</string><key>_CFURLStringType</key><integer>0</integer>' \
+           '</dict></dict></dict>'
 }
 
 start () {
-    command -v brew >/dev/null 2>&1 || { echo >&2 "You don't yet have Homebrew, installing now."; \
+    command -v brew >/dev/null 2>&1 || { purple "You don't yet have Homebrew, installing now."; \
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"; }
     zsh
 }
@@ -15,11 +24,11 @@ start () {
 omz () {
     if [[ -d "/Users/$USER/.oh-my-zsh" ]]
         then 
-            green "You already have Oh-My-Zsh."
+            purple "You already have Oh-My-Zsh."
         else
-            green "Installing Oh-My-Zsh" 
+            purple "Installing Oh-My-Zsh" 
             sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-            green "Going to try to update Oh-My-Zsh for the first time."
+            purple "Going to try to update Oh-My-Zsh for the first time."
             omz update
     fi
     iterm
@@ -28,31 +37,36 @@ omz () {
 zsh () {
     if [ "$SHELL" = "/bin/zsh" ]
         then
-            green "You're using ZSH, going to install Oh-My-Zsh (https://ohmyz.sh)"
+            purple "You're using ZSH, going to install Oh-My-Zsh (https://ohmyz.sh)"
             omz
         else
-            green "You're not using ZSH, going to fix that for you."
+            purple "You're not using ZSH, going to fix that for you."
             brew install zsh
             zsh
     fi
 }
 
 iterm () {
-    green "Installing iTerm2..."
+    purple "Installing iTerm2..."
     brew install iterm2
     theme
 }
 
 theme () {
-    green "Installing theme..."
+    purple "Installing theme..."
     brew install starship
     echo "eval '$(starship init zsh)'" >> ~/.zshrc
     brew tap homebrew/cask-fonts
     brew install --cask font-hack-nerd-font
     curl https://github.com/sindresorhus/iterm2-snazzy/raw/main/Snazzy.itermcolors --output Snazzy.itermcolors 
     open Snazzy.itermcolors
-    green "Theme installed."
-    rm Snazzy.itermcolors
+    purple "Theme installed."
+    sleep 0.5
+    rm ./Snazzy.itermcolors
+    defaults write com.apple.dock \
+        persistent-apps -array "$(__dock_item /Applications/iTerm.app)"
+    purple "Restarting Dock"
+    killall Dock
 }
 
 start
