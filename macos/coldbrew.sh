241c697a86fa9b7ee344b4eb11cc39473e239651
Dylan <alloydylan@icloud.com>
2023-01-20 21:23:11 -0500

    Added Emojis

--- macos/coldbrew.sh
+++ macos/coldbrew.sh
@@ -16,7 +16,7 @@ __dock_item() {
 }
 
 start () {
-    command -v brew >/dev/null 2>&1 || { purple "You don't yet have Homebrew, installing now."; \
+    command -v brew >/dev/null 2>&1 || { purple "⛔️ You don't yet have Homebrew, installing now ⛔️"; \
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"; }
     zsh
 }
@@ -24,11 +24,11 @@ start () {
 omz () {
     if [[ -d "/Users/$USER/.oh-my-zsh" ]]
         then 
-            purple "You already have Oh-My-Zsh."
+            purple "✅ You already have Oh-My-Zsh. ✅"
         else
-            purple "Installing Oh-My-Zsh" 
+            purple "📀 Installing Oh-My-Zsh 📀" 
             sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-            purple "Going to try to update Oh-My-Zsh for the first time."
+            purple "📡 Going to try to update Oh-My-Zsh for the first time 📡"
             omz update
     fi
     iterm
@@ -37,36 +37,38 @@ omz () {
 zsh () {
     if [ "$SHELL" = "/bin/zsh" ]
         then
-            purple "You're using ZSH, going to install Oh-My-Zsh (https://ohmyz.sh)"
+            purple "✅ You're using ZSH, going to install Oh-My-Zsh (https://ohmyz.sh) ✅"
             omz
         else
-            purple "You're not using ZSH, going to fix that for you."
+            purple "⛔️ You're not using ZSH, going to fix that for you. ⛔️"
             brew install zsh
             zsh
     fi
 }
 
 iterm () {
-    purple "Installing iTerm2..."
+    purple "📀 Installing iTerm2... 📀"
     brew install iterm2
     theme
 }
 
 theme () {
-    purple "Installing theme..."
+    purple "📀 Installing theme... 📀"
     brew install starship
     echo "eval '$(starship init zsh)'" >> ~/.zshrc
     brew tap homebrew/cask-fonts
     brew install --cask font-hack-nerd-font
     curl -k https://raw.githubusercontent.com/sindresorhus/iterm2-snazzy/main/Snazzy.itermcolors --output 'Snazzy.itermcolors'
     open Snazzy.itermcolors
-    purple "Theme installed."
+    purple "🎨 Theme installed 🎨"
     sleep 0.5
     rm Snazzy.itermcolors
     defaults write com.apple.dock \
         persistent-apps -array "$(__dock_item /Applications/iTerm.app)"
-    purple "Restarting Dock"
+    purple "🧑‍💻 Restarting Dock 🧑‍💻"
     killall Dock
+    purple "✨ SUCCESS! ✨ Import iterm.json into iTerm to finish."
+    purple "📄 OPTIONAL STEP 📄 change VSCode terminal font to 'Hack Nerd Font Mono'"
 }
 
 start
